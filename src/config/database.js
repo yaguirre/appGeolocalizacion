100b013508b237c4af3944ae6405f194e129693f
yaguirre <yaguirre@eafit.edu.co>
2018-07-27 16:54:17 -0500

    Añadiendo configuración del ambiente producción

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'test';
+    env = process.env.NODE_ENV || 'production';
 
 var config = {
   development: {
