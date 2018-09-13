c16a13bfec67f2ea83663c48b6a3d18c1e14eed6
USUARIO <usuario@MacBook-Pro-de-USUARIO.local>
2018-09-13 14:26:50 -0500

    quitando error, si entra a passport-setup

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -5,15 +5,12 @@ const User = require('../app/models/user');
 
 
 console.log("ENTRA A PASSPORT SETUP");
-fdsfasdfasdfasdfsa
 passport.use(
-    fdsafdasfasdf4324325
     new Auth0Strategy({
         domain: 'marami21.auth0.com',
         callbackURL: '/auth/auth0/redirect', //sitio al que se redirecciona
         clientID: keys.auth0.clientID,
-        //clientSecret: keys.auth0.clientSecret
-        clientSecret:"fdsfasdf"
+        clientSecret: keys.auth0.clientSecret
     }, () => {
         console.log("function fired")
     })
