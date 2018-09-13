ba23f3ffd5323b85116fc2578554614c0b2a2966
USUARIO <usuario@MacBook-Pro-de-USUARIO.local>
2018-09-13 14:08:44 -0500

    añadiendo error en passport-setup para ver si entra al archivo

--- src/config/passport-setup.js
+++ src/config/passport-setup.js
@@ -5,7 +5,9 @@ const User = require('../app/models/user');
 
 
 console.log("ENTRA A PASSPORT SETUP");
+fdsfasdfasdfasdfsa
 passport.use(
+    fdsafdasfasdf4324325
     new Auth0Strategy({
         domain: 'marami21.auth0.com',
         callbackURL: '/auth/auth0/redirect', //sitio al que se redirecciona
