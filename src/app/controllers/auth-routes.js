38d35a47d36adaea91f727820763f87132e1f189
System Administrator <root@MacBook-Pro-de-USUARIO.local>
2018-09-15 14:57:26 -0500

    cambiar

--- src/app/controllers/auth-routes.js
+++ src/app/controllers/auth-routes.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const passport = require('passport');
 
 
-/*router.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
     res.render('login')
-})*/
-router.get('/login',
+})
+router.get('/auth0',
     passport.authenticate('auth0', { scope: 'openid email profile' }), function (req, res) {
         res.redirect('login');
     });
@@ -15,10 +15,6 @@ router.get('/logout', (req, res) => {
     res.send('logging out');    
 });
 
-// auth with auth0
-router.get('/auth0', passport.authenticate('auth0', {
-    scope: ['profile']
-}));
 
 //callback route for auth0 to redirect
 router.get('/auth0/redirect', passport.authenticate('auth0'), (req,res) => {
