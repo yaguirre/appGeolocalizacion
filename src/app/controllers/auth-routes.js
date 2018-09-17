a57c830aac987fa4887162d10ff8cdac5b530f33
System Administrator <root@MacBook-Pro-de-USUARIO.local>
2018-09-16 19:10:32 -0500

    cambiando auth-routes

--- src/app/controllers/auth-routes.js
+++ src/app/controllers/auth-routes.js
@@ -39,7 +39,7 @@ router.get('/auth0', (req, res) => {
 // callback route for google to redirect to
 router.get('/auth0/redirect', passport.authenticate('auth0'), (req, res) => {
     //res.send(req.user);
-    res.redirect('/index/');
+    res.redirect('/');
 
     //res.send('you reached the callback URI');
 })
