c3cf2d8a80e0a9aa83869d51bd2bdbb0c91dc0d9
System Administrator <root@MacBook-Pro-de-USUARIO.local>
2018-09-17 15:15:46 -0500

    añadiendo autenticacion index.js

--- src/app.js
+++ src/app.js
@@ -71,8 +71,16 @@ app.use(expressValidator({
  
 app.use('/auth', authRoutes);
 
-app.get('/', (req, res) => {
-    res.render('index');
+const authCheck = (req, res, next) => {
+    if(!req.user){
+        res.redirect('/auth/login');
+    } else {
+        next();
+    }
+};
+
+app.get('/', authCheck, (req, res) => {
+    res.render('profile', { user: req.user });
 });
 
 
